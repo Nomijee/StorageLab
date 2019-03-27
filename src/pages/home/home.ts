5febac8ac8aaee500be5d01b90fd6e7411b1123e
Nomijee <33327266+Nomijee@users.noreply.github.com>
2019-03-27 15:50:40 +0000

    EmotionLab

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+  
 })
 export class HomePage {
+information:string;
+  constructor(public navCtrl: NavController, private storage: Storage) {
 
-  constructor(public navCtrl: NavController) {
+  }
+  openStatusPage(){
+    this.navCtrl.push('StatusPage');
+    
+  }
 
+  ionViewWillEnter(){
+    this.storage.get("status")
+    .then((val) => {
+      this.information = val;
+    }).catch((err) => {
+      console.log(err);
+    })
   }
 
 }
